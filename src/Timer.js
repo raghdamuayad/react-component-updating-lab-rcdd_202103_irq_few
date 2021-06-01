8980af5135a3dfd5a30468505f6ad1fca1a0e4dc
Raghda  Muayad <therose000777@gmail.com>
2021-06-01 19:07:17 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -11,7 +11,7 @@ class Timer extends Component {
   }
 
   //Your code here
-
+  
   componentDidMount() {
     this.interval = setInterval(
       this.clockTick,
@@ -19,6 +19,15 @@ class Timer extends Component {
     );
   }
 
+  componentDidUpdate() {
+  this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+   }
+  shouldComponentUpdate(nextProps, nextState) {
+  if (this.state.time === nextState.time) {
+  return false;
+  }
+  return true;
+  }
   componentWillUnmount() {
     clearInterval(this.interval);
   }
